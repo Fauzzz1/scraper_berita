064e3c6ee95278640f986830ae8615f894f8e799
Fauzzz1 <vadam1469@gmail.com>
2026-07-06 14:43:58 +0700

    update scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,9 @@
+import sys
+import os
 import schedule
 import time
 import subprocess
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def jalankan_pipeline():
     print("Menjalankan pipeline...")
